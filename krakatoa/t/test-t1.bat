ee9c809625495320f6af085b3d719bfb509cc9a3
igorceridorio <igorceridorio@hotmail.com>
2016-12-14 17:54:08 -0200

    Adding 't' tests folder again

---
+++ krakatoa/t/test-t1.bat
@@ -0,0 +1,233 @@
+REM Primeiro trabalho. Testa todos os ok´s e todos os er's
+
+
+del ok-*.txt
+del er-*.txt
+del *.htm
+del *.html
+del r.txt
+del ok-*.exe
+del er-*.exe
+
+
+
+java -cp bin comp.Comp ..\..\..\ktests\ER-LEX02.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-LEX03.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-LEX04.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-LEX05.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-LEX06.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-LEX07.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-LEX08.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-LEX09.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-LEX11.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM01.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM02.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM03.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM04.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM05.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM06.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM07.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM08.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM09.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM11.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM12.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM13.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM14.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM15.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM16.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM17.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM18.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM19.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM20.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM21.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM22.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM23.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM24.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM25.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM26.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM27.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM28.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM29.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM30.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM31.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM32.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM33.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM34.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM35.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM36.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM37.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM38.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM39.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM40.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM41.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM42.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM43.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM44.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM45.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM46.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM47.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM48.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM49.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM50.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM51.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM57.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM58.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM59.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM60.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM61.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM62.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM63.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM64.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM65.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM66.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM67.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM68.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM69.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM70.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM71.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM72.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM73.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM74.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM75.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM76.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM77.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM78.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM79.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM80.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM81.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM82.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM83.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM84.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SEM85.KRA
+
+
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN01.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN02.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN03.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN04.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN05.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN06.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN07.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN08.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN09.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN10.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN11.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN12.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN13.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN14.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN15.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN16.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN17.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN18.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN19.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN20.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN21.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN22.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN23.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN24.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN25.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN26.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN27.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN28.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN29.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN30.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN31.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN32.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN33.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN34.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN35.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN36.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN37.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN38.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN39.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN40.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN41.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN43.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN44.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN51.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN52.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN53.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN54.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN55.KRA
+java -cp bin comp.Comp ..\..\..\ktests\ER-SIN56.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-CHK01.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-CHK02.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-CHK03.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-CHK04.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-CHK05.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-CHK06.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-CHK07.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER01.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER02.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER03.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER04.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER05.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER06.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER07.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER08.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER09.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER10.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER11.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER12.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER14.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER15.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER16.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER17.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER18.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER19.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER20.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER21.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-GER22.KRA
+
+
+java -cp bin comp.Comp ..\..\..\ktests\OK-LEX02.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-LEX03.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-LEX04.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-LEX05.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-LEX06.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-LEX07.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-LEX08.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-LEX10.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM03.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM04.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM05.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM06.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM07.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM08.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM09.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM10.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM11.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM12.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM13.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM14.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM15.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM17.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM18.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM19.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SEM20.KRA
+
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN02.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN03.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN04.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN05.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN06.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN07.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN08.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN09.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN10.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN11.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN12.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN13.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN14.KRA
+java -cp bin comp.Comp ..\..\..\ktests\OK-SIN15.KRA
+
+
+cmd /C java -cp ..\..\..\soft\autoT at
+
+
+del *.tds
+del *.obj
+del *.bak
+del ok*.exe
+del er*.exe
+
